4fa93e7b84a99e98aecf0f51a00ea0ce51cab899
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-08-30 14:50:04 +0200

    Add methods to History class
    add push and pop methods.

--- src/com/legion/memento/History.java
+++ src/com/legion/memento/History.java
@@ -7,5 +7,15 @@ public class History {
 
     private List<MovieState> states = new ArrayList<>();
 
+    public void push(MovieState state) {
+        states.add(state);
+    }
 
+    public MovieState pop() {
+        int lastIndex = states.size()-1;
+        MovieState lastState = states.get(lastIndex);
+        states.remove(lastState);
+
+        return lastState;
+    }
 }
