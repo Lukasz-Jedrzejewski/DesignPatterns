c3ef953d5a453b64c114ab6fc69d3bc7b42f1ef6
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-08-31 13:21:45 +0200

    Add Main class for testing results.

---
+++ src/com/legion/state/Main.java
@@ -0,0 +1,17 @@
+package com.legion.state;
+
+public class Main {
+    public static void main(String[] args) {
+        GameController controller = new GameController();
+        /**
+         * Using polymorphism
+         */
+        controller.setCurrentButton(new UpButton());
+        controller.press();
+        controller.release();
+
+        controller.setCurrentButton(new DownButton());
+        controller.press();
+        controller.release();
+    }
+}
