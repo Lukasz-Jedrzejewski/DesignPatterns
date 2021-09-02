1ea728be5ac41d81a1b05b0fe54b703ef98ac898
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-09-02 15:34:41 +0200

    Add PngCompressor class
    Create PngCompressor class with implementation of Compressor interface method.

---
+++ src/com/legion/strategy/PngCompressor.java
@@ -0,0 +1,9 @@
+package com.legion.strategy;
+
+public class PngCompressor implements Compressor {
+
+    @Override
+    public void compress(String fileName) {
+        System.out.println(String.format("Compressing file %s using PNG"));
+    }
+}
