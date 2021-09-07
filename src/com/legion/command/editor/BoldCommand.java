0181f959ea42d24087526aa834ae5fbf4a9329a1
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-09-07 17:04:02 +0200

    Add BoldCommand class
    Add fields and constructor.
    Add execute() and unExecute() methods implementation.

---
+++ src/com/legion/command/editor/BoldCommand.java
@@ -0,0 +1,25 @@
+package com.legion.command.editor;
+
+public class BoldCommand implements UndoableCommand {
+
+    private String previousContent;
+    private HtmlDocument document;
+    private History history;
+
+    public BoldCommand(HtmlDocument document, History history) {
+        this.document = document;
+        this.history = history;
+    }
+
+    @Override
+    public void unExecute() {
+        document.setContent(previousContent);
+    }
+
+    @Override
+    public void execute() {
+        previousContent = document.getContent();
+        document.makeBold();
+        history.push(this);
+    }
+}
